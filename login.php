9b27f036afbf75ba0af21738c5df0ba18f854dbc
ChamaliHemapala <39118168+ChamaliHemapala@users.noreply.github.com>
2018-05-09 20:43:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
 <?php
 
-	
+	//start session
 	session_start();
+	
+	//storing session id
 	$sessionID=session_id();
-	setcookie("session_id",$sessionID,time()+3600,"/","localhost",false,true);
+	setcookie("session_id",$sessionID,time()+3600,"/","localhost",false,true); // setting cookie with the session id
 	
 	
 ?>
@@ -16,7 +18,9 @@
 </style>
 </head>
 <body> 
-<script> 
+
+<!-- Function requesting token from validate.php. You can refer w3school for this !-->
+<script>
 function loadDOC(method,url,htmlTag)
 {
     var xhttp = new XMLHttpRequest(); //create variable for store HTTP requests
@@ -35,17 +39,10 @@ function loadDOC(method,url,htmlTag)
     xhttp.open(method,url,true);
     xhttp.send();
 }
-</script>
-<?php 
-	if (isset($_COOKIE['session_id']))
-	{
-		echo '<script> loadDOC("POST","validate.php","cs");
-		</script>';
-		
-		
-	}
 
-?>
+
+
+ </script>
 
 <h2>PHP Form Login Example</h2>
 <p><span class="error">* required field</span></p>
@@ -60,11 +57,24 @@ function loadDOC(method,url,htmlTag)
   <input type="checkbox" name="remember" value=1>
   Remember Me
   <br><br>
-  <input type="hidden" id="cs" name="csrf" >
+  <input type="hidden" id="cs" name="csrf"/> <!--DOM hidden field creation!-->
 
   <input type="submit" name="login" value="login">  
 </form>
 
+<!--Calling the loadDOC function to get the token!-->
+<?php 
+	if (isset($_COOKIE['session_id']))
+	{
+		echo '<script> loadDOC("POST","validate.php","cs");
+		</script>';
+		
+		
+	}
+
+?>
+
+<!-- This is a additional part apart from csrf protection. This is the cookie which created for the email when you tick the remember me check box, so the next time you log in and if the cookie is there stored in the browser yoye email will be there in the email field!-->
 <?php
 	if(isset($_COOKIE['email'])  )
 	{	
